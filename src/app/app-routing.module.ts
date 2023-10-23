17f2da42dd056ef3a7ebeaf28fb54a44c179b674
Renatter <riko321riko321@gmail.com>
2023-10-23 07:55:57 +0100

    fix:app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { WorkshopComponent } from '../pages/workshop/workshop.component';
-import { ToolsPageComponent } from '../pages/tools-page/tools-page.component';
-const routes: Routes = [
-  {path:'workshop', component: WorkshopComponent},
-  {path:'tools', component: ToolsPageComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
