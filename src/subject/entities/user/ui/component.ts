122a0bb7b28706aa9268996992af5766c2143e98
Renatter <riko321riko321@gmail.com>
2023-11-06 07:17:16 +0000

    fix: исправил ошибку

---
+++ src/subject/entities/user/ui/component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-info-entities',
+  templateUrl: './content.html',
+  styleUrls: ['./style.css'],
+})
+export class UserProfileComponent {
+
+}
