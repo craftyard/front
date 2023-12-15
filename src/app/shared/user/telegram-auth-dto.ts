513294886ca1fadb38907ab046f06758c27af00a
Renatter <riko321riko321@gmail.com>
2023-12-15 07:44:06 +0000

    fix: изменил telegram-auth-dto

--- src/app/shared/user/telegram-auth-dto.ts
+++ src/app/shared/user/telegram-auth-dto.ts
@@ -1,9 +1,10 @@
+// Этот тип данных поддублирован в бэкэнде
 export type TelegramAuthDTO = {
   id: number,
-  first_name: string,
-  Last_name: string,
-  username: string,
-  photo_url: string,
+  first_name?: string,
+  Last_name?: string,
+  username?: string,
+  photo_url?: string,
   auth_date: string,
   hash: string,
 }
