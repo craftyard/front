1e15ede2fa0734f962cd8922c7b29defe0de663a
Renatter <riko321riko321@gmail.com>
2023-12-26 17:13:19 +0600

    fetaute: создал popup button

--- src/workshop/widgets/wokrshop-widgets/ui/component.ts
+++ src/workshop/widgets/wokrshop-widgets/ui/component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddModelComponent } from 'workshop/feature/add-model/ui/component';
+import { AddTollComponent } from 'workshop/feature/add-tools/ui/component';
 
 @Component({
   selector: 'workshop-widget',
@@ -10,11 +11,26 @@ import { AddModelComponent } from 'workshop/feature/add-model/ui/component';
 export class WorkshopWidgetsComponent {
   constructor(public dialog: MatDialog) {}
 
-  openDialog() {
-    const dialogRef = this.dialog.open(AddModelComponent);
+  menuItems = [
+    { title: 'Добавить модель', link: AddModelComponent },
+    { title: 'Добавить инструменты', link: AddTollComponent },
+  ];
+
+  menuData: string = 'Добавить модель';
+
+  menuLink?: unknown;
+
+  openDialog(menuLink: any = AddModelComponent): void {
+    const dialogRef = this.dialog.open(menuLink);
 
     dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
     });
+  }
+
+  selectMenuItem(item: any): void {
+    this.menuData = item.title;
+    this.menuLink = item.link;
+    this.openDialog(item.link);
   }
 }
