fa72ccf102779607a304deafd6a0ca50c49a2b8f
Nuradil <yaksh0nti@gmail.com>
2023-12-27 14:34:18 +0500

    изменен название пакета и зависимостей и исправлены импорты

--- src/subject/shared/backend-api/subject-api.service.ts
+++ src/subject/shared/backend-api/subject-api.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Authentificationable } from 'app/shared/user/authentificationable';
-import { JWTTokens } from 'rilata2/src/app/jwt/types';
+import { JWTTokens } from 'rilata/src/app/jwt/types';
 import { Observable } from 'rxjs';
-import { TelegramAuthDTO } from 'workshop-domain/src/subject/domain-data/user/user-authentification/a-params';
+import { TelegramAuthDTO } from 'cy-domain/src/subject/domain-data/user/user-authentification/a-params';
 
 @Injectable({
   providedIn: 'root',
