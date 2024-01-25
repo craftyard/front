747d37724b0ae6cd26060cd8e31ea52bc04d59d0
Renatter <riko321riko321@gmail.com>
2024-01-25 15:41:07 +0600

    fix:исправил путь router

--- src/model/module-state.ts
+++ src/model/module-state.ts
@@ -3,7 +3,7 @@ import { DomainModuleState } from '../app/shared/states/domain-module-state';
 
 export class ModelModuleState implements DomainModuleState {
   private treeItems:TreeItem[] = [
-    { title: 'модели', routerLink: 'modelspage' },
+    { title: 'модели', routerLink: 'models' },
   ];
 
   getTreeItems(): TreeItem[] {
